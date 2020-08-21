e8679b3d33ce42a8b3771749d2825b3d5fa5621d
projetoUno <provaetb@gmail.com>
2020-08-21 20:45:15 -0300

    Finalizando o desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,49 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, tech} = request.body
+ const repository = {id: uuid(), title, url, tech, like:0};
+ repositories.push(repository);
+
+ return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const {title, url, tech} = request.body
+const repositorieIndex = repositories.findIndex(res => res.id === id)
+if(repositorieIndex < 0){
+    return response.status(400).json({error: 'repositorie not found.'})
+}
+const repositorie = {id, title, url, tech}
+console.log(request.body);
+repositories[repositorieIndex] = repositorie;
+    return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositorieIndex = repositories.findIndex(res => res.id === id)
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: 'repositorie not found.'})
+}
+repositories.splice(repositorieIndex,1);
+return response.status(204).json()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositorieIndex = repositories.findIndex(res => res.id === id)
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: 'repositorie not found.'})
+  }
+repositories[repositorieIndex].like++;
+return response.json(repositories[repositorieIndex])
 });
+
 
 module.exports = app;
